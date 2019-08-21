b9e12bdfa54b3cd798af94853241c15a661a9478
Eric Hein <ehein6@gatech.edu>
2019-08-21 09:33:57 -0400

    Fix atomic_cas to work for doubles

--- emu_cxx_utils/intrinsics.h
+++ emu_cxx_utils/intrinsics.h
@@ -22,11 +22,37 @@ atomic_addms(T * volatile * ptr, ptrdiff_t value)
 
 template<class T>
 inline T
-atomic_cas(T volatile * ptr, T oldval, T newval)
+atomic_cas(T volatile * ptr, T oldval, T newval);
+
+template<>
+inline long
+atomic_cas(long volatile * ptr, long oldval, long newval)
 {
-    static_assert(sizeof(T) == 8, "ATOMIC_CAS requires 64-bit type");
-    return (T)ATOMIC_CAS((volatile long*)ptr, (long)oldval, (long)newval);
+    return ATOMIC_CAS(ptr, oldval, newval);
 }
+
+template<>
+inline double
+atomic_cas(double volatile * ptr, double oldval, double newval)
+{
+    // TODO create an ATOMIC_CAS intrinsic that takes a double
+    // We're fighting the C++ type system, but codegen doesn't care
+    union pun {
+        double f;
+        long i;
+    };
+    pun oldval_p{oldval};
+    pun newval_p{newval};
+    pun retval_p;
+
+    retval_p.i = ATOMIC_CAS(
+        reinterpret_cast<long volatile*>(ptr),
+        oldval_p.i,
+        newval_p.i
+    );
+    return retval_p.f;
+}
+
 
 //TODO implement all remotes/atomics
 };
