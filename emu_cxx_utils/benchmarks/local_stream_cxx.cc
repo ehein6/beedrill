231d2c9501a6a25a079e83e617cfd5b68142011e
Eric Hein <ehein@emutechnology.com>
2019-10-14 18:34:59 -0400

    Clean up global/local stream
    Use default grain size for fill() also

--- emu_cxx_utils/benchmarks/local_stream_cxx.cc
+++ emu_cxx_utils/benchmarks/local_stream_cxx.cc
@@ -1,10 +1,8 @@
 #include <vector>
-#include <emu_cxx_utils/for_each.h>
+#include <memory>
 #include <emu_cxx_utils/fill.h>
-#include <emu_cxx_utils/execution_policy.h>
 #include <emu_cxx_utils/transform.h>
 #include <common.h>
-#include <memory>
 
 using namespace emu;
 using namespace emu::execution;
@@ -16,14 +14,14 @@ struct stream {
     void init()
     {
         // forall i, A[i] = 1, B[i] = 2, C[i] = -1
-        parallel::fill(fixed, a_.begin(), a_.end(), 1L);
-        parallel::fill(fixed, b_.begin(), b_.end(), 2L);
-        parallel::fill(fixed, c_.begin(), c_.end(), -1L);
+        parallel::fill(a_.begin(), a_.end(), 1L);
+        parallel::fill(b_.begin(), b_.end(), 2L);
+        parallel::fill(c_.begin(), c_.end(), -1L);
     }
 
     void run()
     {
-        parallel::transform(fixed, a_.begin(), a_.end(), b_.begin(), c_.begin(),
+        parallel::transform(a_.begin(), a_.end(), b_.begin(), c_.begin(),
             [] (long a, long b) {
                 return a + b;
             }
--- emu_cxx_utils/fill.h
+++ emu_cxx_utils/fill.h
@@ -3,12 +3,15 @@
 #include <emu_cxx_utils/for_each.h>
 namespace emu::parallel {
 
-template<typename Policy, typename Iterator, typename T>
+template<typename ExecutionPolicy, typename Iterator, typename T,
+    // Disable if first argument is not an execution policy
+    std::enable_if_t<execution::is_execution_policy_v<ExecutionPolicy>, int> = 0
+>
 void
-fill(Policy&& policy, Iterator first, Iterator last, const T& value)
+fill(ExecutionPolicy policy, Iterator first, Iterator last, const T& value)
 {
-    emu::parallel::for_each(std::forward<Policy>(policy), first, last,
-        [&](T& item){ item = value; }
+    for_each(policy, first, last,
+        [value](T& item){ item = value; }
     );
 }
 
