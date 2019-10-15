572591ec84ce424398237f9b71f509fa31c03d45
Eric Hein <ehein6@emutechnology.com>
2019-10-15 11:18:32 -0400

    Initial reduce() and transform_reduce()

--- emu_cxx_utils/intrinsics.h
+++ emu_cxx_utils/intrinsics.h
@@ -53,6 +53,50 @@ atomic_cas(double volatile * ptr, double oldval, double newval)
     return retval_p.f;
 }
 
+struct remote_add {
+    void operator()(long &lhs, const long &rhs) { REMOTE_ADD(&lhs, rhs); }
+};
+
+struct remote_and {
+    void operator()(long &lhs, const long &rhs) { REMOTE_AND(&lhs, rhs); }
+};
+
+struct remote_or {
+    void operator()(long &lhs, const long &rhs) { REMOTE_OR(&lhs, rhs); }
+};
+
+struct remote_xor {
+    void operator()(long &lhs, const long &rhs) { REMOTE_XOR(&lhs, rhs); }
+};
+
+struct remote_min {
+    void operator()(long &lhs, const long &rhs) { REMOTE_MIN(&lhs, rhs); }
+};
+
+struct remote_max {
+    void operator()(long &lhs, const long &rhs) { REMOTE_MAX(&lhs, rhs); }
+};
+
+template<typename F>
+struct remote_op {
+};
+
+template<>
+struct remote_op<std::plus<long>> : remote_add {
+};
+template<>
+struct remote_op<std::bit_and<long>> : remote_and {
+};
+template<>
+struct remote_op<std::bit_or<long>> : remote_or {
+};
+template<>
+struct remote_op<std::bit_xor<long>> : remote_xor {
+};
+
+// Problem: there are no std functors for min and max. They might even be macros.
+// What will binary_op be if the programmer wants a max/min reduction?
+// Probably they should use std::max_element or std::min_element instead
 
 //TODO implement all remotes/atomics
 };
---
+++ emu_cxx_utils/transform_reduce.h
@@ -0,0 +1,115 @@
+#pragma once
+
+#include <transform.h>
+#include <reduce.h>
+
+#include <boost/iterator/transform_iterator.hpp>
+#include <boost/iterator/zip_iterator.hpp>
+
+namespace emu::parallel {
+namespace detail {
+
+template<class ExecutionPolicy, class ForwardIt, class T, class BinaryOp, class UnaryOp>
+T transform_reduce(
+    ExecutionPolicy &&policy,
+    ForwardIt first, ForwardIt last,
+    T init, BinaryOp binary_op, UnaryOp unary_op) {
+    // Create transform iterator with unary_op
+    auto first_t = boost::make_transform_iterator(first, unary_op);
+    auto last_t = boost::make_transform_iterator(last, unary_op);
+
+    // Reduce using binary_op
+    return reduce(policy, first_t, last_t, init, binary_op);
+}
+
+template<class ExecutionPolicy, class ForwardIt1, class ForwardIt2, class T, class BinaryOp1, class BinaryOp2>
+T transform_reduce(
+    ExecutionPolicy &&policy,
+    ForwardIt1 first1, ForwardIt1 last1, ForwardIt2 first2,
+    T init, BinaryOp1 binary_op1, BinaryOp2 binary_op2) {
+    // Compute end of second range
+    auto last2 = std::advance(first2, std::distance(first1, last1));
+    // Zip ranges together
+    auto first = boost::make_zip_iterator(
+        std::make_tuple(first1, first2));
+    auto last = boost::make_zip_iterator(
+        std::make_tuple(last1, last2));
+    // Helper function to map the binary op onto a tuple
+    auto worker = [](auto tuple) {
+        return binary_op2(std::get<0>(tuple), std::get<1>(tuple));
+    };
+    // Convert to transform iterator
+    auto first_t = boost::make_transform_iterator(first, worker);
+    auto last_t = boost::make_transform_iterator(last, worker);
+
+    // Reduce using binary_op1
+    return reduce(policy, first_t, last_t, init, binary_op1);
+}
+
+} // end namespace detail
+
+// Wrappers to provide default arguments
+
+template<class ExecutionPolicy,
+    class ForwardIt1, class ForwardIt2, class T,
+    class BinaryOp1, class BinaryOp2,
+    // Disable if first argument is not an execution policy
+    std::enable_if_t<execution::is_execution_policy_v<ExecutionPolicy>, int> = 0
+>
+T transform_reduce(ExecutionPolicy &&policy,
+                   ForwardIt1 first1, ForwardIt1 last1, ForwardIt2 first2,
+                   T init, BinaryOp1 binary_op1, BinaryOp2 binary_op2) {
+    return detail::transform_reduce(
+        policy, first1, last1, first2, init, binary_op1, binary_op2
+    );
+}
+
+template<class ExecutionPolicy,
+    class ForwardIt, class T, class BinaryOp, class UnaryOp,
+    // Disable if first argument is not an execution policy
+    std::enable_if_t<execution::is_execution_policy_v<ExecutionPolicy>, int> = 0
+>
+T transform_reduce(ExecutionPolicy &&policy,
+                   ForwardIt first, ForwardIt last,
+                   T init, BinaryOp binary_op, UnaryOp unary_op) {
+    return detail::transform_reduce(
+        policy, first, last, init, binary_op, unary_op
+    );
+}
+
+template<class ExecutionPolicy, class ForwardIt1, class ForwardIt2, class T,
+    // Disable if first argument is not an execution policy
+    std::enable_if_t<execution::is_execution_policy_v<ExecutionPolicy>, int> = 0
+>
+T transform_reduce(ExecutionPolicy &&policy,
+                   ForwardIt1 first1, ForwardIt1 last1, ForwardIt2 first2, T init) {
+    return detail::transform_reduce(
+        policy, first1, last1, init, std::plus<>(), std::multiplies<>());
+}
+
+template<class InputIt1, class InputIt2, class T>
+T transform_reduce(InputIt1 first1, InputIt1 last1, InputIt2 first2, T init) {
+    return detail::transform_reduce(
+        execution::default_policy, first1, last1, init,
+        std::plus<>(), std::multiplies<>()
+    );
+}
+
+template<class InputIt1, class InputIt2, class T, class BinaryOp1, class BinaryOp2>
+T transform_reduce(InputIt1 first1, InputIt1 last1, InputIt2 first2,
+                   T init, BinaryOp1 binary_op1, BinaryOp2 binary_op2) {
+    return detail::transform_reduce(execution::default_policy, first1, last1,
+        first2, init, binary_op1, binary_op2);
+}
+
+template<class InputIt, class T, class BinaryOp, class UnaryOp>
+T transform_reduce(InputIt first, InputIt last,
+                   T init, BinaryOp binary_op, UnaryOp unary_op)
+{
+    return detail::transform_reduce(execution::default_policy, first, last,
+        init, binary_op, unary_op);
+}
+
+
+
+} // end namespace emu::parallel
