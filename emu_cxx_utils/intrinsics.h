f74fe25681762fbc34279a89038a3274b8c43718
Eric Hein <ehein@emutechnology.com>
2019-11-08 15:25:39 -0500

    Add remote add instrinsic

--- emu_cxx_utils/intrinsics.h
+++ emu_cxx_utils/intrinsics.h
@@ -25,6 +25,7 @@ template<class T>
 inline T
 atomic_cas(T volatile * ptr, T oldval, T newval);
 
+// Compare-and-swap on 64-bit int
 template<>
 inline long
 atomic_cas(long volatile * ptr, long oldval, long newval)
@@ -32,34 +33,12 @@ atomic_cas(long volatile * ptr, long oldval, long newval)
     return ATOMIC_CAS(ptr, newval, oldval);
 }
 
-template<>
-inline double
-atomic_cas(double volatile * ptr, double oldval, double newval)
-{
-    // TODO create an ATOMIC_CAS intrinsic that takes a double
-    // We're fighting the C++ type system, but codegen doesn't care
-    union pun {
-        double f;
-        long i;
-    };
-    pun oldval_p{oldval};
-    pun newval_p{newval};
-    pun retval_p;
-
-    retval_p.i = ATOMIC_CAS(
-        reinterpret_cast<long volatile*>(ptr),
-        newval_p.i,
-        oldval_p.i
-    );
-    return retval_p.f;
-}
-
+// Compare-and-swap on 64-bit type
 template<typename T>
 inline T
 atomic_cas(T volatile * ptr, T oldval, T newval)
 {
     static_assert(sizeof(T) == sizeof(long), "CAS supported only for 64-bit types");
-    // TODO create an ATOMIC_CAS intrinsic that takes a double
     // We're fighting the C++ type system, but codegen doesn't care
     union pun {
         T t;
@@ -77,50 +56,22 @@ atomic_cas(T volatile * ptr, T oldval, T newval)
     return retval_p.t;
 }
 
-struct remote_add {
-    void operator()(long &lhs, const long &rhs) { REMOTE_ADD(&lhs, rhs); }
-};
 
-struct remote_and {
-    void operator()(long &lhs, const long &rhs) { REMOTE_AND(&lhs, rhs); }
-};
+// Remote add on 64-bit int
+inline void
+remote_add(volatile long * ptr, long value)
+{
+    return REMOTE_ADD(ptr, value);
+}
 
-struct remote_or {
-    void operator()(long &lhs, const long &rhs) { REMOTE_OR(&lhs, rhs); }
-};
-
-struct remote_xor {
-    void operator()(long &lhs, const long &rhs) { REMOTE_XOR(&lhs, rhs); }
-};
-
-struct remote_min {
-    void operator()(long &lhs, const long &rhs) { REMOTE_MIN(&lhs, rhs); }
-};
-
-struct remote_max {
-    void operator()(long &lhs, const long &rhs) { REMOTE_MAX(&lhs, rhs); }
-};
-
-template<typename F>
-struct remote_op {
-};
-
-template<>
-struct remote_op<std::plus<long>> : remote_add {
-};
-template<>
-struct remote_op<std::bit_and<long>> : remote_and {
-};
-template<>
-struct remote_op<std::bit_or<long>> : remote_or {
-};
-template<>
-struct remote_op<std::bit_xor<long>> : remote_xor {
-};
-
-// Problem: there are no std functors for min and max. They might even be macros.
-// What will binary_op be if the programmer wants a max/min reduction?
-// Probably they should use std::max_element or std::min_element instead
+// Remote add on pointer
+template<class T>
+inline T*
+remote_add(T * volatile * ptr, ptrdiff_t value)
+{
+    value *= sizeof(T);
+    REMOTE_ADD((volatile long*)ptr, (long)value);
+}
 
 //TODO implement all remotes/atomics
 };
