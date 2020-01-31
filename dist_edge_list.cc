a7b5058c9ba08469861e9eb3bda807bad7ebd649
Eric Hein <ehein@emutechnology.com>
2020-01-31 10:36:36 -0500

    Use more C++ like syntax

--- dist_edge_list.cc
+++ dist_edge_list.cc
@@ -238,7 +238,7 @@ dist_edge_list::load(const char* filename)
     LOG("Loading %li edges from %s\n", header.num_edges, filename);
 
     // Double-buffering: read edges into one buffer while we scatter the other
-    size_t buffer_len = (100 * (1 << 20)) / sizeof(edge);
+    size_t buffer_len = 65536;
     std::vector<edge> buffer_A(buffer_len);
     std::vector<edge> buffer_B(buffer_len);
     auto* file_buffer = &buffer_A;
