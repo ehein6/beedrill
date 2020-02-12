9d6065b0376a6c3542c08bbba9a83e94d45e2fd2
Eric Hein <ehein@emutechnology.com>
2020-02-12 18:11:14 -0500

    Remove old files

--- sorting.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void emu_quick_sort_longs(long * begin, long * end, int (*compare)(const void *, const void *));
-int is_sorted(long * begin, long * end, int (*compare)(const void *, const void *));
